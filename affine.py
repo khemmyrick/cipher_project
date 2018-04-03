59a68d1face32fb981cf1251414ee6ba16174942
Khem Myrick <khem.myrick@gmail.com>
2018-04-03 14:37:36 -0700

    Updated affine.py
    Moved Affine Cipher functionality into class format. 1 cipher down, 2 to go.

--- affine.py
+++ affine.py
@@ -1,107 +1,78 @@
 import string
 
-input_string = "PALACE"
-output_string = ""
-affine_num = []
-key_a = 5
-key_b = 8
+from ciphers import Cipher
 
-alpha = string.ascii_uppercase
-affine_cip = ""
-for char in alpha:
-    affine_cip += (alpha[(key_a * alpha.index(char) + key_b) % 26])
+class Affine(Cipher):
+    """The Affine Cipher."""
+    # alpha = string.ascii_uppercase
+    ALPHA = string.ascii_uppercase
+    
+    def __init__(self):
+        """Initializes instance of Affine Cipher."""
+#        self.key_a = key_a
+#        self.key_b = key_b
+        self.ALPHA = string.ascii_uppercase
+        
+    def encrypt(self, text, key_a, key_b):
+        """Encrypt a string, via instance.encrypt(string)."""
+        affine_cip = ""
+        output_string = ""
+        text = text.upper()
+        for char in self.ALPHA:
+            affine_cip += (self.ALPHA[(key_a * self.ALPHA.index(char) + key_b) % 26])
+        alpha_dict = {letter: number for number, letter in zip(range(0,26), self.ALPHA)}
+        affine_dict = {number: letter for letter, number in zip(affine_cip, range(0,26))}
+        for char in text:
+            output_string += affine_dict[alpha_dict[char]]
+
+        return output_string
+
+    
+    def decrypt(self, text, key_a, key_b):
+        """Decrypt a string, via instance.decrypt(string)."""
+        affine_cip = ""
+        output_string = ""
+        text = text.upper()
+        for char in self.ALPHA:
+            affine_cip += (self.ALPHA[(key_a * self.ALPHA.index(char) + key_b) % 26])
+        rev_affine_dict = {number: letter for letter, number in zip(range(0,26), affine_cip)}
+        rev_alpha_dict = {letter: number for number, letter in zip(self.ALPHA, range(0,26))}
+        for char in text:
+            output_string += rev_alpha_dict[rev_affine_dict[char]]            
+        return output_string
+        
+        # input_string = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
+# output_string = ""
+# affine_num = []
+# key_a = 5
+# key_b = 8
+
+# alpha = string.ascii_uppercase
+# affine_cip = ""
+# for char in alpha:
+#    affine_cip += (alpha[(key_a * alpha.index(char) + key_b) % 26])
 
 # Compare each letter in input string to each letter in full alphabet,
 # Return corresponding letter in cipher.
     
-print(input_string)
-alpha_dict = {letter: number for number, letter in zip(range(0,26), alpha)}
-
-affine_dict = {number: letter for letter, number in zip(affine_cip, range(0,26))}
-
-for char in input_string:
-    output_string += affine_dict[alpha_dict[char]]
-    
-print(output_string)
-#affine_num.append
-#for char in alpha_list:
-#    if char in alpha:
-#        affine_num.append(alpha.index(char))
-
-#print(affine_num)
-
+# print(input_string)
 # input_string -> alpha_index [int list] -> cipher_index [int list] -> output_string
+# alpha_dict = {letter: number for number, letter in zip(range(0,26), alpha)}
+
+# affine_dict = {number: letter for letter, number in zip(affine_cip, range(0,26))}
+
+# for char in input_string:
+#    output_string += affine_dict[alpha_dict[char]]
+    
+# print(output_string)
+
+# new_output = ""
+# rev_affine_dict = {number: letter for letter, number in zip(range(0,26), affine_cip)}
+
+# rev_alpha_dict = {letter: number for number, letter in zip(alpha, range(0,26))}
 
 
-
-
-
-
-#affine_num += str
-#print(alpha.index(char for char in input_string))
-#print(affine_num)
-#output_string += affine_cip[num for num in affine_num]
-
-
-#print(output_string)
-#class Affine(Cipher):
-#    """The Affine Cipher."""
-#    FORWARD = string.ascii_uppercase
-    # Sets variable FORWARD to 3 sets of entire all-caps alphabet.
-
-#    def __init__(self, key_a, key_b):
-#        """Initializes an instance of the Affine cipher.
-#        This instance can be fed a string to encrypt.
-#        And this same instance (or any instance of Caesar cipher)
-#        can be used to decrypt said strings.
-#        """
-#        self.key_a = key_a
-#        self.key_b = key_b
-        # Sets instance.offset to offset argument, in this case, "3".
-        
-#        for i in range(26):
-
-#            self.FORWARD = string.ascii_uppercase + string.ascii_uppercase[:self.offset+1]
-
-
-
-        # Sets instance.FORWARD to all-caps alphabet (plus alpha to offset (which is 3) plus 1).
-#        self.BACKWARD = string.ascii_uppercase[:self.offset+1] + string.ascii_uppercase
-        # Sets instance.BACKWARD to an all caps offset + 1, plus full alphabet.
-
-#####
-#def affine(a, b):
-#    for i in range(26):
-        #transpo_table = {chr(i+65): chr(((a*i+b)%26)+65)}
-#        print({string.ascii_uppercase: (a * string.ascii_uppercase * b)%26})
-        #print(transpo_table)
-
-#An example call
-#affine(5, 8)
-
-
-
-# from ciphers import Cipher
-# Implement 3 ciphers.  Each with its own class and module.
-# Docstrings for all functions/methods.
-#class Affine(Cipher):
-#    """The Affine Cipher."""
-    # *** for demonstration purposes, from wikipedia page on affine cipher.
-    #Prints a transposition table for an affine cipher.
-    #a must be coprime to m=26.
-    # if we add 0 - 9 to cipher, m will be 36.
-    # key_a can never be any even integer or 13 with an m of 26.
-    # for an m=36, key_a can  never be any even integer or 3.
-#    def __init__(self, key_a, key_b):
-        # a and b are the key of the cipher.
-        # key_b can be anything, as long as key_a isn't 1.
-
-#        for i in range(m=26):
-            #if (key_a * (key_a ^ -1)) % m == 1:
-#            return(chr(i+65) + ": " + chr(((key_a*i+key_b)%26)+65))
-
-#An example call
-#affine(5, 8)
-
-# *** end wikipedia code.
-
+# for char in output_string:
+#    new_output += rev_alpha_dict[rev_affine_dict[char]]
+    
+# print(new_output)
