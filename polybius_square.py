6fdcfc0880b7e0456662b11207ea533176624653
Khem Myrick <khem.myrick@gmail.com>
2018-04-06 22:37:43 -0700

    Added decrypt for Polybius Square.
    I experienced a succession of "Oh yeah! I knew that!" moments.

--- polybius_square.py
+++ polybius_square.py
@@ -1,28 +1,85 @@
 import string
 
 
-
 ALPHANUM = string.ascii_uppercase + '0123456789'
 rows = range(6)
 cols = range(6)
 GRID = [(x, y) for y in rows for x in cols]
 AZ_NUMGRID = dict(zip(ALPHANUM, GRID))
+NUM_AZ = dict([[valu,keey] for keey,valu in AZ_NUMGRID.items()])
+AZ_NUMLIST = str(AZ_NUMGRID)
+AZ_NUMLIST = AZ_NUMLIST.replace("), (", " ")
+AZ_NUMLIST = AZ_NUMLIST.replace(", ", "")
+AZ_NUMLIST = AZ_NUMLIST.replace("[(", "")
+AZ_NUMLIST = AZ_NUMLIST.replace(")]", "")
+#print(AZ_NUMLIST)
 #print(AZ_NUMGRID)
+#print(NUM_AZ)
 
 def encrypt(text):
+    print(text)
     output_string = ''
     poly_cip = []
-    polyanna = []
     for char in text:
-        poly_cip.append(AZ_NUMGRID[char])
+        try:
+            poly_cip.append(AZ_NUMGRID[char])
+        except KeyError:
+            if char == " ":
+                poly_cip.append("  ") 
+            else:
+                poly_cip.append(char)
+            continue
     output_string = str(poly_cip)
     output_string = output_string.replace("), (", " ")
     output_string = output_string.replace(", ", "")
     output_string = output_string.replace("[(", "")
     output_string = output_string.replace(")]", "")
+    output_string = output_string.replace(")'", "")
+    output_string = output_string.replace("'(", "")
     print(output_string)
 
-encrypt("AQUADRY")
+        
+
+encrypt("CRYSTAL GEMS")
+
+def decrypt(text):
+    """Decrypts code.  Input must be all numbers."""
+
+    #text = str(text)
+    output_string = ''
+    poly_cip = []
+    input_list = []
+    output_list = []
+    #for text[i] in text:
+    #    input_list.append(int(text[i]))
+    #print(input_list)
+    input_list.append(text.split())
+    itera = len(text)
+    while itera > 0:
+        temp_tup = (int(text[0]), int(text[1]))
+        # print(temp_tup)
+        output_list.append(NUM_AZ[temp_tup])
+        #output_list.extend(temp_tup)
+        # print(output_list)
+        text = text.replace(text[:2], '', 1)
+        itera -= 2
+
+    output = ''.join(output_list)
+    print(output)
+    
+decrypt('00422300305204')
+    #for pair in input_list:
+    #    [k for (k, v) in AZ_NUMGRID.iteritems() if v == 0]
+
+    #for char in text:
+    #    poly_cip.append(AZ_NUMGRID[char])
+    #output_string = str(poly_cip)
+    #output_string = output_string.replace("), (", " ")
+    #output_string = output_string.replace(", ", "")
+    #output_string = output_string.replace("[(", "")
+    #output_string = output_string.replace(")]", "")
+    #print(output_string)    
+
 #from ciphers import Cipher
 
 #class Polybius_square(Cipher):
